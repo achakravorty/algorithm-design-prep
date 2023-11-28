c94d8288900a57faea9dba349bfc2d8615cf9a63
Ashika Chakravorty <ashika.chak@outlook.com>
2023-11-28 21:55:07 +0100

    Greedy algorithms (weblab) - Can We Get Out (COMPLETED - 18/18)

---
+++ src/main/java/weblab/greedy/CanWeGetOut.java
@@ -0,0 +1,48 @@
+package weblab.greedy;
+
+import java.util.*;
+
+class Node {
+
+    List<Node> outgoingEdges;
+
+    boolean marked;
+
+    public Node() {
+        this.outgoingEdges = new ArrayList<>();
+        this.marked = false;
+    }
+}
+
+public class CanWeGetOut {
+    /**
+     * @param nodes the nodes in the graph
+     * @param s the starting node
+     * @param t the final node
+     * @return true iff there is a path from the start node to the final node
+     */
+    public static boolean solve(Set<Node> nodes, Node s, Node t) {
+        // use BFS to check if there exists a path between s and t
+        Deque<Node> q = new ArrayDeque<>();
+
+        q.add(s);
+        s.marked = true;
+
+        while(!q.isEmpty()) {
+            Node p = q.remove();
+            if (p.equals(t)) {
+                return true;
+            }
+
+            for (Node neighbour : p.outgoingEdges) {
+                if (!neighbour.marked) {
+                    q.add(neighbour);
+                    neighbour.marked = true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
---
+++ src/test/java/weblab/greedy/CanWeGetOutTest.java
@@ -0,0 +1,35 @@
+package weblab.greedy;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+class CanWeGetOutTest {
+
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    public void example() {
+        int n = 7;
+        Node[] nodesArr = new Node[n + 1];
+        Set<Node> nodes = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            nodesArr[i] = new Node();
+            nodes.add(nodesArr[i]);
+        }
+        Node s = nodesArr[1];
+        Node t = nodesArr[5];
+        nodesArr[1].outgoingEdges.add(nodesArr[2]);
+        nodesArr[2].outgoingEdges.add(nodesArr[3]);
+        nodesArr[3].outgoingEdges.add(nodesArr[4]);
+        nodesArr[4].outgoingEdges.add(nodesArr[5]);
+        nodesArr[2].outgoingEdges.add(nodesArr[6]);
+        nodesArr[6].outgoingEdges.add(nodesArr[7]);
+        nodesArr[7].outgoingEdges.add(nodesArr[4]);
+        Assertions.assertTrue(CanWeGetOut.solve(nodes, s, t));
+        Assertions.assertFalse(CanWeGetOut.solve(nodes, t, s));
+    }
+
+}
